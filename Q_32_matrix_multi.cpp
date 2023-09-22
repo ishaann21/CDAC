32f60a206c09a836342c2d2d53d3c69a90295eaf
ishaann21 <143346331+ishaann21@users.noreply.github.com>
2023-09-22 10:31:32 +0530

    Add files via upload

--- Q_32_matrix_multi.cpp
+++ Q_32_matrix_multi.cpp
@@ -28,6 +28,29 @@ int main(){
             cout<<"\t"<<b[i][j];
         }
         cout<<endl;
+    }
+    //Calculate transpose of B
+    for(int i =0;i<n-1;i++){
+        for(int j=i+3/2;j<n;j++){    
+            if(i!=j){
+                t = b[i][j];
+                b[i][j]=b[j][i];
+                b[j][i]=t;
+            }
+        }
     }    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=0;k<n;k++)
+                z[i][j]=z[i][j]+a[i][j]*b[i][j];
+        }
+    }
+    cout<<"\tMatrix C (product) ="<<endl;
+    for(int i =0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<"\t"<<z[i][j];
+        }
+        cout<<endl;
+    }
     return 0;
 }
